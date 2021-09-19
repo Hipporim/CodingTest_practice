1cc0a687f07622bdc4f3a6e2421abd70664d6356
turtleYeRim <82985546+turtleYeRim@users.noreply.github.com>
2021-09-20 01:22:05 +0900

    Create 두 정수 사이의 합.c

---
+++ 두 정수 사이의 합.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int temp;
+    if(a>b){
+        temp=b;
+        b=a;
+        a=temp;
+    }
+    for(int i=a; i<=b; i++){
+        answer+=i;
+    }
+    return answer;
+}
